a4a9bb491465ed4529aff45579826353c72b5606
Samoth69 <11927133+Samoth69@users.noreply.github.com>
2026-08-09 18:27:53 +0200

    fix(cross-seed): add sonarr-tv

--- clusters/k4/kubernetes/apps/media/cross-seed/app/config/config.js
+++ clusters/k4/kubernetes/apps/media/cross-seed/app/config/config.js
@@ -22,7 +22,10 @@ module.exports = {
     searchCadence: "1 day",
     skipRecheck: true,
     // radarr: [`http://radarr.media.svc.cluster.local:7878/?apikey=${process.env.RADARR__AUTH__APIKEY}`],
-    sonarr: [`https://sonarr-anime.${process.env.SECRET_DOMAIN}/?apikey=${process.env.SONARR_ANIME__AUTH__APIKEY}`],
+    sonarr: [
+        `https://sonarr-anime.${process.env.SECRET_DOMAIN}/?apikey=${process.env.SONARR_ANIME__AUTH__APIKEY}`,
+        `https://sonarr-tv.${process.env.SECRET_DOMAIN}/?apikey=${process.env.SONARR_TV__AUTH__APIKEY}`
+    ],
     torrentClients: [`qbittorrent:${process.env.QBITTORRENT_PROXY_URL}`],
     torznab: fetchIndexers(`https://prowlarr.${process.env.SECRET_DOMAIN}`, process.env.PROWLARR__AUTH__APIKEY, "cross-seed"),
     useClientTorrents: true
